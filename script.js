0f8cfca5d5e6be48d9caf86d0bc4e9e016d149c2
axisrin <sumipix.sky@mail.ru>
2022-11-06 03:42:55 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+let header__burger = document.querySelector('.header__burger');
+let header_menu = document.querySelector('.header__menu');
+let back = document.querySelector('body');
+let header__list = document.querySelector('.header__list');
+let header = document.querySelector('.header');
+
+header__burger.onclick = function(){
+    if (header__burger.classList.contains('active') === false)
+    {
+        header__burger.classList.toggle('active');
+        header_menu.classList.toggle('active');
+        back.classList.toggle('lock');
+    }
+    else {
+        header__burger.classList.remove('active');
+        header_menu.classList.remove('active');
+        back.classList.remove('lock');
+    }
+}
+
+header__list.onclick = function () {
+    back.classList.remove('lock');
+}
+
+let color = '#00d2d2';
+let times = setTimeout( function () {
+    $('.content__header h1 , .page__header h1').css('box-shadow', '0px 2px 0px 0px' + color);
+}, 0)
+
+let timer = setInterval(function () {
+    if (color === '#00d2d2') {
+            color = '#ff6e91';
+        } else if (color === '#ff6e91') {
+            color = '#00d2d2';
+        }
+        $('.content__header h1 , .page__header h1').css('box-shadow', '0px 2px 0px 0px' + color);
+    }, 3100);
+
+let lineTimes = setInterval(function () {
+    $('.content__hr-top').css('width', '20%');
+}, 1000)
+
+let btn_line = document.querySelector('.button-for-js-1');
+
+btn_line.onmouseover = btn_line.onmouseout = handler;
+
+function handler(event) {
+    if (event.type == 'mouseover') {
+        $('.content__hr-top').css('width', '20%');
+    }
+}
